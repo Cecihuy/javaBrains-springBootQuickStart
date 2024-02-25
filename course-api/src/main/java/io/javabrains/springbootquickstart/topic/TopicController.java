d2c98539878afdb9baa0fdc5fa57a91d5d3b3f65
sourceTree <sourcetree@mail.com>
2024-02-25 14:45:46 -0700

    19 - Creating a new resource using POST

--- course-api/src/main/java/io/javabrains/springbootquickstart/topic/TopicController.java
+++ course-api/src/main/java/io/javabrains/springbootquickstart/topic/TopicController.java
@@ -2,7 +2,9 @@ package io.javabrains.springbootquickstart.topic;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +20,9 @@ public class TopicController {
     @RequestMapping(path = "/topics/{id}")
     public Topic getTopic(@PathVariable String id){
         return topicService.getTopic(id);
+    }
+    @RequestMapping(method = RequestMethod.POST, path = "/topics")
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
     }
 }
--- course-api/src/main/java/io/javabrains/springbootquickstart/topic/TopicService.java
+++ course-api/src/main/java/io/javabrains/springbootquickstart/topic/TopicService.java
@@ -1,15 +1,16 @@
 package io.javabrains.springbootquickstart.topic;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<Topic>(Arrays.asList(
                 new Topic("spring", "Spring Framework", "Spring Framework Description"),
                 new Topic("java", "Core Java", "Core Java Description"),
                 new Topic("javascript", "Java Script", "Java Script Description")
-            );
+            ));
     public List<Topic> getAllTopics(){
         return topics;
     }
@@ -18,5 +19,8 @@ public class TopicService {
             .filter(t -> t.getId().equals(id))
             .findFirst()
             .get();
+    }
+    public void addTopic(Topic topic) {
+        topics.add(topic);
     }
 }
